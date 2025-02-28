56bb99b18263be68059be579df67aba35df3ac24
Raphael Mauricio <hargenx@hotmail.com>
2025-02-27 21:22:02 -0300

    Extra

---
+++ 08_ARA0075_PPS_NA/aula02/exercicio05.java
@@ -0,0 +1,14 @@
+public class exercicio05 {
+
+    public static float fatorial(int n) {
+        int fat = 1;
+        for (int i = 1; i <= n; i++) {
+            fat *= i;
+        }
+        return fat;
+    }
+    public static void main(String[] args) {
+        int n = Integer.parseInt(System.console().readLine("Digite o valor para o calculo do Fatorial: "));
+        System.out.println("Fatorial de " + n +  " = " + fatorial(n));
+    }
+}
