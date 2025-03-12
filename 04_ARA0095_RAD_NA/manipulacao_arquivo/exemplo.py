7ea1757505676b8647383517bba66b7d74abe9ac
Raphael Mauricio <hargenx@hotmail.com>
2025-03-11 21:08:48 -0300

    exemplo

--- 04_ARA0095_RAD_NA/manipulacao_arquivo/exemplo.py
+++ 04_ARA0095_RAD_NA/manipulacao_arquivo/exemplo.py
@@ -5,3 +5,16 @@ print('Modo do arquivo: ', arquivo.mode)
 print('Arquivo está fechado? ', arquivo.closed)
 arquivo.close()
 print('Arquivo está fechado? ', arquivo.closed)
+import os
+# Diretório base
+diretorio_base = 'D:\\Estacio\\2025_01\\codigos\\04_ARA0095_RAD_NA'
+# Subdiretório e nome do arquivo
+subdiretorio = 'manipulando_arquivo'
+nome_arquivo = 'dados.txt'
+# Construir o caminho relativo
+caminho_relativo = os.path.join(diretorio_base, subdiretorio, nome_arquivo)
+# Obter o caminho absoluto
+caminho_absoluto = os.path.abspath(caminho_relativo)
+# Exibir os resultados
+print(f'Caminho relativo: {caminho_relativo}')
+print(f'Caminho absoluto: {caminho_absoluto}')
