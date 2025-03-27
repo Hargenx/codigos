0d9d18284043e6e70ae0f854b846389fc730f618
Raphael Mauricio <hargenx@hotmail.com>
2025-03-27 18:30:24 -0300

    muda

--- A08_ARA0075_PPS_NA/aula06/CloneWars/MainPrototypeCopy.java
+++ A08_ARA0075_PPS_NA/aula06/CloneWars/MainPrototypeCopy.java
@@ -4,13 +4,14 @@ public class MainPrototypeCopy {
     public static void main(String[] args) {
         // Cria uma pessoa original com um endereço.
         Endereco endereco = new Endereco("Rua A");
-        Pessoa original = new Pessoa("João", endereco);
+        Pessoa original = new Pessoa("Raphael", endereco);
 
         // Cria clones usando shallow copy e deep copy.
         Pessoa cloneShallow = original.cloneShallow();
         Pessoa cloneDeep = original.cloneDeep();
 
         System.out.println("Antes de modificar:");
+
         System.out.println("Original: " + original);
         System.out.println("Clone Shallow: " + cloneShallow);
         System.out.println("Clone Deep: " + cloneDeep);
@@ -18,7 +19,7 @@ public class MainPrototypeCopy {
         // Modifica o endereço do clone shallow.
         cloneShallow.getEndereco().setRua("Rua B");
         // Modifica o nome do clone deep para diferenciar.
-        cloneDeep.setNome("Maria");
+        cloneDeep.setNome("Caroline");
 
         System.out.println("\nDepois de modificar o cloneShallow:");
         System.out.println("Original: " + original);
