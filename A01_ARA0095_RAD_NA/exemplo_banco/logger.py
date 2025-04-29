ee41878ac0c13d610633c05e955667a95c6f8fa7
Raphael Mauricio <hargenx@hotmail.com>
2025-04-29 16:35:06 -0300

    correto

--- A01_ARA0095_RAD_NA/exemplo_banco/logger.py
+++ A01_ARA0095_RAD_NA/exemplo_banco/logger.py
@@ -1,8 +1,15 @@
 import logging
+import os
 
+caminho = os.path.dirname(os.path.abspath(__file__))
+# Verifica se o diretório de logs existe, caso contrário, cria um novo
+if not os.path.exists(caminho):
+    os.makedirs(caminho)
+nome = "app.log"
+log = os.path.join(caminho, nome)
 # Configuração básica do logger
 logging.basicConfig(
-    filename="app.log",
+    filename=log,
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s",
 )
