38973974dc39d1701b4f500953840877ca4e9804
Raphael <Hargenx@hotmail.com>
2025-06-02 00:21:16 -0300

    fck

--- A05_ARA0089_Mobile_NA/projetos/src/types/player.ts
+++
@@ -1,9 +0,0 @@
-import { BSON } from 'realm';
-
-export interface Player {
-    _id: BSON.ObjectId;
-    name: string;
-    position: 'Goleiro' | 'Defensor' | 'Meio-campo' | 'Atacante' | 'Indefinido';
-    skillLevel: 1 | 2 | 3 | 4 | 5; // Nível de 1 a 5
-    createdAt: Date;
-}
